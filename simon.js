f5c8f04edd69816b2d8bc3b3c75fbd9cab25ad04
MatiasTexmer <matiastosoexmer@gmail.com>
2025-06-16 20:23:49 -0300

    Juego finalizado, falta testing

--- simon.js
+++ simon.js
@@ -1,29 +1,56 @@
 let secuenciaMaquina = [];
 let secuenciaUsuario = [];
-const COLORES = ['rojo', 'amarillo', 'azul', 'verde'];
 
+const $ronda = document.querySelector('#ronda');
+let ronda = 0;
+const $estado = document.querySelector('#estado');
+const COLORES = ['rojo', 'amarillo', 'azul', 'verde'];
+reiniciar();
 document.querySelector('#comenzar').addEventListener('click', function () {
-    // la maquina debe elegir un color aleatorio
+    // la maquina debe elegi r un color aleatorio
     manejarSeleccionMaquina();
 });
 
 function manejarSeleccionMaquina() {
+    actualizarEstado('Turno de la maquina...');
     const color = elegirColorAleatorio(COLORES);
     secuenciaMaquina.push(color);
-    console.log(document.querySelector('.' + color));
     console.log('maquina', color);
-    resaltarCuadro(color);
+
+    incrementarRonda();
+
+    for (let i = 0; i < secuenciaMaquina.length; i++) {
+        setTimeout(() => { resaltarCuadro(secuenciaMaquina[i]); }, "1000" * (i + 1));
+    }
+
+    setTimeout(function () {
+        desbloquearUsuario();
+        actualizarEstado('Turno del jugador...');
+    }, "1000" * secuenciaMaquina.length)
+
 }
 
-const $colores = document.querySelectorAll('.color');
-for (let i = 0; i < $colores.length; i++) {
-    const $color = $colores[i];
-    $color.addEventListener('click', function () {
-        const color = this.dataset.color;
-        manejarSeleccionUsuario(color);
 
-    })
+function bloquearUsuario() {
+    const $colores = document.querySelectorAll('.color');
+    for (let i = 0; i < $colores.length; i++) {
+        const $color = $colores[i];
+        $color.onclick = function () { };
+    }
+};
+
+function desbloquearUsuario() {
+    const $colores = document.querySelectorAll('.color');
+    for (let i = 0; i < $colores.length; i++) {
+        const $color = $colores[i];
+        $color.onclick = function () {
+            const color = this.dataset.color;
+            manejarSeleccionUsuario(color);
+
+        }
+    }
 }
+
 
 function resaltarCuadro(color) {
     document.querySelector('.' + color).style.opacity = 1;
@@ -34,13 +61,10 @@ function resaltarCuadro(color) {
 function manejarSeleccionUsuario(color) {
     console.log('usuario', color);
     secuenciaUsuario.push(color);
-
+    resaltarCuadro(color);
     if (color !== secuenciaMaquina[secuenciaUsuario.length - 1]) {
-        console.log('Perdiste');
-        secuenciaMaquina = [];
-        secuenciaUsuario = [];
+        reiniciar(true);
         return;
-
     }
 
     if (secuenciaUsuario.length === secuenciaMaquina.length) {
@@ -53,3 +77,28 @@ function elegirColorAleatorio(colores) {
     return colores[indiceAleatorio];
 }
 
+function incrementarRonda() {
+    $ronda.textContent = ++ronda;
+
+}
+
+function reiniciarRonda() {
+    ronda = 0;
+    $ronda.textContent = ronda;
+}
+
+function actualizarEstado(estado) {
+    $estado.textContent = estado;
+}
+
+function reiniciar(usuarioPerdio = false) {
+    if (usuarioPerdio) {
+        actualizarEstado('Perdiste! Toca "Comenzar" para reiniciar el juego.')
+    } else {
+        actualizarEstado('Toca Comenzar" para iniciar el juego')
+    }
+    bloquearUsuario();
+    secuenciaMaquina = [];
+    secuenciaUsuario = [];
+    reiniciarRonda();
+}
